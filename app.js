50325b7b7204633449e9d7ec4e84af3b4ae09768
Valera Mikhalkevich <korolfantom14@gmail.com>
2022-05-07 14:14:50 +0300

    HW-14

---
+++ app.js
@@ -0,0 +1,23 @@
+function createAddress(city, street) {
+        return {city, street}
+}
+function createPerson(id, name, address) {
+        return {id, name, address};
+}
+const persons = [
+        createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
+        createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
+        createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
+        createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
+]
+function getPersonsCity(persons, city) {
+        return persons.filter(p => p.address.city === city);
+}
+console.log(getPersonsCity(persons, 'Rehovot'));
+//task 4
+function movePersonsNoCityAtBeginning(persons, city) {
+        const personsRes = getPersonsCity(persons, city);
+        personsRes.unshift(...persons.filter(p => p.address.city !== city));
+        return personsRes;
+}
+console.log(movePersonsNoCityAtBeginning(persons,  'Rehovot'));
